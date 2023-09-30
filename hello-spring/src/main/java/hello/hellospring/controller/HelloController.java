fbd243bac415a3eb25f55efda456514a0eb93862
You Jeong Jang <sunny819819@naver.com>
2023-09-30 23:18:11 +0900

    Update HelloController.java

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,11 +3,43 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 public class HelloController {
     @GetMapping("hello")
     public String hello(Model model){
         model.addAttribute("data","hello!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc") //1) mvc
+    public String helloMvc(@RequestParam(value = "name", required = true) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string") //2) API
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api") //3) API JSON , 객체
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
