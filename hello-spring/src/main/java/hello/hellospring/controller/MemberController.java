6c18cc313f9a603b8223dc3483fe9ae323680f47
You Jeong Jang <sunny819819@naver.com>
2023-10-02 23:53:59 +0900

    Update MemberController.java
    Postmapping 추가

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,10 @@
 package hello.hellospring.controller;
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -31,4 +33,11 @@ public class MemberController {
         return "members/createMemberForm";
     }
 
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
 }
