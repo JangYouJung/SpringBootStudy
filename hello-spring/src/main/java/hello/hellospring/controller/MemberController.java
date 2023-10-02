b7c56b46124856c8aa18a954962da95c013ed536
You Jeong Jang <sunny819819@naver.com>
2023-10-03 00:07:26 +0900

    Update MemberController.java
    회원 정보 조회 기능 추가

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -3,8 +3,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -28,16 +31,26 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    //[1]회원 등록 버튼 눌렀을 때
     @GetMapping(value = "/members/new")
     public String createForm() {
         return "members/createMemberForm";
     }
 
+    //[2]회원 정보 입력 후 가입 버튼 눌렀을 때 (회원 정보 담고 있으므로 Post mapping)
     @PostMapping(value = "/members/new")
     public String create(MemberForm form) {
         Member member = new Member();
         member.setName(form.getName());
         memberService.join(member);
         return "redirect:/";
+    }
+
+    //[3]회원 정보 조회
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
