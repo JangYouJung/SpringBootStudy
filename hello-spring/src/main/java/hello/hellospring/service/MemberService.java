af0a72a32386dcfa6ed7cd4924633be9dd98ea6d
You Jeong Jang <sunny819819@naver.com>
2023-10-09 22:31:11 +0900

    AOP 전: 수기로 시간 출력 코드 추가

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -44,9 +44,29 @@ public class MemberService {
 
      // [3] 중복 회원 검색 세 번째 코드: findByName을 Extract Method로 따로 빼줌
     public Long join(Member member) {
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
+    }
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
@@ -55,13 +75,6 @@ public class MemberService {
                 });
     }
 
-
-    /**
-     * 전체 회원 조회
-     */
-    public List<Member> findMembers() {
-        return memberRepository.findAll();
-    }
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
     }
